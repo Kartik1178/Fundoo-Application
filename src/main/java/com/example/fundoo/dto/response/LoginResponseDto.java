10936252f82fc848ee5bcae2f992d7b7822dfc96
Kartikeya Pusapati <swagboss600@gmail.com>
2026-04-10 11:06:34 +0530

    [Kartikeya]: UC-1 - add request and response DTOs for User and Note

---
+++ src/main/java/com/example/fundoo/dto/response/LoginResponseDto.java
@@ -0,0 +1,13 @@
+﻿package com.example.fundoo.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginResponseDto {
+    private String token;
+    private String message;
+}
---
+++ src/main/java/com/example/fundoo/dto/response/UserResponseDto.java
@@ -0,0 +1,16 @@
+﻿package com.example.fundoo.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserResponseDto {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String message;
+}
